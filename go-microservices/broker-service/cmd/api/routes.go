9cdb678adde9fdad0c11813fdd11d2dffad64fd5
measutosh <crispyasutoshpanda3@gmail.com>
2022-09-02 07:21:11 +0000

    adding go-microservices initial phase

---
+++ go-microservices/broker-service/cmd/api/routes.go
@@ -0,0 +1,30 @@
+// this contains all the routes for the application
+package main
+
+import (
+	"net/http"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+// here (app *Config) is the receiever that comes from main.go
+func (app *Config) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	// mention who is allowed to connect
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:     []string{"http://*", "https://*"},
+		AllowedMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:     []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:     []string{"Link"},
+		AllowCredentials:   true,
+		MaxAge:             300,
+	}))
+
+	// for a aliveness check-up, this should respond something to prove it's active
+	mux.Use(middleware.Heartbeat("/ping"))
+
+	// no handlers yet so returning mux
+	return mux
+}
